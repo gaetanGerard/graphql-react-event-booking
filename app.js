33fe1aa439380f66e5b67c8db9b212ed281dfca8
Gaetan.gerard <gaetan.gerard@data-essential.com>
2020-07-10 14:36:04 +0200

    Add graphql, express and mongo DB to project

--- app.js
+++ app.js
@@ -1,8 +1,139 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/user');
+const user = require('./models/user');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(3000);
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then((results) => {
+            return results.map((event) => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch((err) => console.log(err));
+      },
+      createEvent: (args) => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: '5f085bc40d465b64c4a26230',
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then((result) => {
+            createdEvent = { ...result._doc, _id: event._doc._id.toString() };
+            return User.findById('5f085bc40d465b64c4a26230');
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error('User Not found.');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((result) => {
+            console.log(result);
+            return createdEvent;
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error('User exist already.');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch((err) => {
+            throw err;
+          });
+      },
+    },
+    graphiql: true,
+  }),
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ffwrr.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
+
+/* Chapter 7 of mongoDB/graphql/nodejs/react
+    https://www.youtube.com/watch?v=bgq7FRSPDpI&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_&index=7
+  */
